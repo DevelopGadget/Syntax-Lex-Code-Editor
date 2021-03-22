0fc91dd9b2d73ad4d56e012af45d747baaf970a5
Fernando Araujo <developgadget@gmail.com>
2021-03-21 19:46:20 -0500

    Fix subir

--- src/app/utils/variableUtil.ts
+++ src/app/utils/variableUtil.ts
@@ -67,8 +67,8 @@ export class VariableUtil {
   validateOptionsVariable(variable: string): string {
     variable = variable.trim();
     if (/^(?:(?!([a-zA-Z]))(.*))*$/.test(variable)) return 'Error nombre de variable no inicia con una letra "' + variable + '"\n';
-    else if (/(?:(?!(\w+)).+)$/.test(variable)) return 'Error nombre de variable no debe contener caracteres especiales "' + variable + '"\n';
     else if (/^([a-zA-Z])(\s+)(\w+)$/.test(variable)) return 'Error nombre no debe contener espacios "' + variable + '"\n';
+    else if (/(?:(?!(\w+)).+)$/.test(variable)) return 'Error nombre de variable no debe contener caracteres especiales "' + variable + '"\n';
     else if (/^(declare|entero|cadena|logico|fecha|real|entonces|mq|finmq|para|finpara|haga|recibe|si|finsi|sino|function|Inicio|Fin|envia|recibe|llamar)$/.test(variable)) return 'Error nombre de variable no debe contener palabras reservadas "' + variable + '"\n';
     else if (!variable || variable.length <= 0 || variable.length > 16) return 'Error nombre de variable tiene que ser mínimo 1 y máximo 16 "' + variable + '"\n';
     else return null;
